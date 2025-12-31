300c0c8b2b7372040c8878f1f14150fb91794640
andredale-lab <andreadalessandro167@gmail.com>
2025-12-31 11:43:34 +0100

    analitycs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { Analytics } from '@vercel/analytics/react'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+    <Analytics />
+  </React.StrictMode>
+)
